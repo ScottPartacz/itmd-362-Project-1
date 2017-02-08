00b5ad5a4794a3acb6fc36623f0d49f538a84bce
ScottPartacz <Sparatcz@hawk.iit.edu>
2017-02-08 10:15:34 -0600

    added more to js to hide the submit butten until you have a vaild email

--- js/site.js
+++ js/site.js
@@ -4,5 +4,16 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 
-
+  $('#email').on('keyup', function(){
+    var currentvalue = $(this).val();
+    var vaildpattern = $(this).attr('pattern');
+    console.log(currentvalue,vaildpattern);
+    if(currentvalue.match(vaildpattern)){
+      $('#submit').addClass('active');
+    }
+    else {
+      $('#submit').removeClass('active');
+    }
+  });
+  
 });
