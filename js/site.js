b7744887bde6550ed51522235c6e55b4f7a5bee1
ScottPartacz <Sparatcz@hawk.iit.edu>
2017-03-02 23:48:12 -0600

    added patterns for name and fav movie also so the start of my js file

--- js/site.js
+++ js/site.js
@@ -1,18 +1,25 @@
 $.noConflict();
-
-jQuery(function($) {
+(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-
-  $('#email').on('keyup focus blur', function() {
-    var currentValue = $(this).val();
-    var validPattern = $(this).attr('pattern');
-    console.log(currentValue,validPattern);
-    if(currentValue.match(validPattern)) {
-      $('#submit').addClass('active');
-    } else {
-      $('#submit').removeClass('active');
-    }
-  });
-
-});
+  
+  $(document).ready(function(){
+    $('#movies').on('submit', function(e) { 
+      e.preventDefault(); 
+      window.location.reload();
+      $("#name").val("");
+      $("#email").val("");
+      $("#fav").val("");
+      }); 
+      $('#email').on('keyup focus blur', function() {
+       var currentValue = $(this).val();
+       var validPattern = $(this).attr("pattern");
+       if (currentValue.match(validPattern)) {
+          $("#submit").addClass("active");
+        } 
+        else {
+          $("#submit").removeClass("active");
+        }
+      });
+     });
+})(jQuery);
